1bc001473f32e8006848c1f0c41c2fc192b148ab
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-29 00:53:43 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,38 +1,40 @@
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
-        }
-        for (int item : data) {
-    }
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-
-        }
-}
-def generate_random_data():
-    return data
-        print(f"Random Number: {item}")
-def main():
-
-    data = [random.randint(1, 100) for _ in range(10)]
 
     data = generate_random_data()
-import random
-if __name__ == "__main__":
-
-
-    main()
-    for item in data:        print(f"Random Number: {item}")
+def generate_random_data():
 def main():
 if __name__ == "__main__":
-    data = generate_random_data()
-import random
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    main()
-
     return data
+
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
     for item in data:
+    main()
+        print(f"Random Number: {item}")
+    main()
+        print(f"Random Number: {item}")
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
 
+
+def generate_random_data():
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+import random
+
+
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+import random
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+
+def main():
+
+def generate_random_data():
+    main()
